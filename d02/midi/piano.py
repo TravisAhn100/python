9040a502fe546357472bdbc67100a1fa65c39be6
anto5710 <anto5710@gmail.com>
2023-08-07 00:10:16 +0900

    예제로 파이썬 피아노 구현

---
+++ d02/midi/piano.py
@@ -0,0 +1,27 @@
+import musicalbeeps    as Mb
+import multiprocessing as mp
+import time
+
+p = Mb.Player(volume = 0.5)
+
+def play_notes(notes_list):
+        print(notes_list)
+        processes_list = []
+        for (i, note) in enumerate(notes_list):
+                if note == '_':
+                        time.sleep(0.5)
+                else:
+                        t = mp.Process(target = p.play_note,\
+                                        args = (note, 1.5))
+                        t.start()
+                        processes_list += [t]
+        for process in processes_list:
+                process.join()
+
+if __name__ == "__main__":
+        while True:
+                chords_strs  = input("> ").split()
+                chords_chars = [list(s) for s in chords_strs]
+
+                for chord in chords_chars:
+                        play_notes(chord)
