d20491d779206a6a7c3dde27e159295514f60246
TravisAhn100 <ashow3685@gmail.com>
2023-08-09 12:58:22 +0900

    s

--- d03/chessboard.py
+++ d03/chessboard.py
@@ -14,10 +14,16 @@ def draw_board(board):
         line = chr(ord('1') + y)
         for (x, cell) in enumerate(row):
             line += ' '
-            if (random() < 0.5):
-                line += BLACK_BG
-            else:
-                line += WHITE_BG
+            if y%2==1:
+                if x%2==0:
+                    line += BLACK_BG
+                if x%2==1:
+                    line += WHITE_BG
+            if y%2==0:
+                if x%2==0:
+                    line += WHITE_BG
+                if x%2==1:
+                    line += BLACK_BG
             line += cell
         line += RESET
         print(line)
@@ -31,29 +37,40 @@ knight='♘'
 
 board[0][ 1]=knight
 board[0][-2]=knight
+board[0][-3]=bishop
+board[0][2]=bishop
+board[0][0]=rook
+board[0][7]=rook
 
 draw_board(board)
 # for line in board:
 #     print(line)
+while True:
+    selection = list(input())
+    x = int(ord(selection[0].lower())) - 97
+    y = int(selection[1])-1
 
-selection = list(input())
-row = ord(selection[0].lower()) - 97
-rank = int(selection[1])-1
-
-print(f"row: {row}, rank: {rank}")
-
-if board[rank][row]=='.':
-    print('.')
-if board[rank][row]=='♘':
-    print('selected')
-    movement = list(input())
-    row2 = ord(movement[0].lower()) - 97
-    rank2 = int(movement[1])-1
-
-
-    print(f"row2: {row2}, rank2: {rank2}")
-
-    board[rank][row]='.'
-    board[rank2][row2]='♘'
-    for line in board:
-        print(line)
+    print(f"row: {x+1}, rank: {y+1}")
+    if y>=8:
+        print('invalid move')
+        continue
+    if board[y][x]=='.':
+        print('blank')
+    if board[y][x]=='♘':
+        print('knight %s selected'%selection)
+        movement = list(input())
+        x2 = ord(movement[0].lower()) - 97
+        y2 = int(movement[1])-1
+        board[y][x]='.'
+        board[y2][x2]='♘'
+        draw_board(board)
+    if board[y][x]=='♖':
+        print('rook %s selected' % str(selection))
+        movement = list(input())
+        x2 = ord(movement[0].lower()) - 97
+        y2 = int(movement[1])-1
+        print(f"row2: {x2+1}, rank2: {y2+1}")
+        board[y][x]='.'
+        board[y2][x2]='♖'
+        draw_board(board)
+        
