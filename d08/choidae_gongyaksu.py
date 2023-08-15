6cdb4ee550bae61fa9db40ee41d15836bef32cca
TravisAhn100 <ashow3685@gmail.com>
2023-08-15 14:30:00 +0900

    commit suicide

--- d08/choidae_gongyaksu.py
+++ d08/choidae_gongyaksu.py
@@ -1,3 +1,18 @@
-# def largest_num(num):
-#     i=2
-#     while num%i==0:
+def largest_num(num):
+    i       = 2
+    largest = 1
+
+    while num % i == 0:
+        num     = num // i
+        largest = i
+
+    for i in range(3, num + 1, 2):
+        while num % i == 0:
+            num     = num // i
+            largest = i
+        if (num <= 1):
+            break
+
+    return largest
+
+print(largest_num(600851475143))
